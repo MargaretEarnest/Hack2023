3274ca0e18da65f16a6e857ae22556e0491859c9
Jakob Misbach <jakobmisbach8@gmail.com>
2023-01-14 11:17:57 -0500

    Added Job Manager

--- backend/src/database/JobDatabaseManager.java
+++ backend/src/database/JobDatabaseManager.java
@@ -1,9 +1,12 @@
 package database;
 
+import jsonObjects.Job;
 import utils.Constants;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class JobDatabaseManager {
 
@@ -39,5 +42,52 @@ public class JobDatabaseManager {
             System.out.println("couldn't connect!");
         }
         return null;
+    }
+
+    public void addJob(Job job){
+        try{
+            Statement statement = Objects.requireNonNull(getConnection()).createStatement();
+            statement.executeUpdate(String.format("INSERT INTO Jobs (ID, Title, Department, Location, StudentsRequired, WeeklyHours, FederalFundingRequired, ContactName, Email, Phone, JobDescription, Requirements) VALUES" +
+                    "(UUID(), '%s', '%s', '%s', %d, %d, %b, '%s', '%s', '%s', '%s', '%s'"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateJob(Job job){
+        try{
+            Statement statement = Objects.requireNonNull(getConnection()).createStatement();
+            statement.executeUpdate(String.format("UPDATE Jobs SET Title = '%s', Department = '%s', Location = '%s', StudentsRequired = %d, WeeklyHours = %d, FederalFundingRequired = %b, ContactName = '%s', Email = '%s', Phone = '%s', JobDescription = '%s', Requirements = '%s' WHERE ID = '%s'",
+                    job.getTitle(), job.getDepartment(), job.getLocation(), job.getNumStudents(), job.getHours(), job.isFederalWorkStudy(), job.getContact(), job.getEmail(), job.getPhone(), job.getDesc(), job.getRequirements(), job.getId()));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Job getJob(String id){
+        try{
+            Statement statement = Objects.requireNonNull(getConnection()).createStatement();
+            ResultSet result = statement.executeQuery("Select * from Jobs WHERE ID = '" + id + "'");
+            if(result.getFetchSize() > 0 && result.first()){
+                return new Job(id, result.getString("Title"), result.getString("Department"), result.getString("Location"), result.getInt("StudentsRequired"), result.getInt("WeeklyHours"), result.getString("Email"), result.getBoolean("FederalFundingRequired"), result.getString("JobDescription"), result.getString("Requirements"), result.getString("Phone"), result.getString("ContactName"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public List<String> getAllJobsIds(){
+        try{
+            Statement statement = Objects.requireNonNull(getConnection()).createStatement();
+            ResultSet results = statement.executeQuery(String.format("SELECT ID FROM Jobs"));
+            List<String> ids = new ArrayList<>();
+            while(results.next()){
+                ids.add(results.getString("ID"));
+            }
+            return ids;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- backend/src/jsonObjects/Job.java
+++ backend/src/jsonObjects/Job.java
@@ -13,10 +13,7 @@ specific job attributes including:
 import utils.HashList;
 
 public class Job {
-    int id; // unique id for this job
-    HashList<Employer> creators; // employers overseeing the job
-    int type; // 0=research, 1=TA, 2=other
-    HashList<String> majors; // majors required for student
+    String id; // unique id for this job
     String title; // name of the job position
     String department; // department name
     String location; // name of the location
@@ -25,24 +22,118 @@ public class Job {
     String email; // contact email
     boolean federalWorkStudy; // if it counts for federal work study
     String desc; // description of the job position
-    float gpa; // required GPA to apply
-    int classYear; // required class year
-    int grad; // 0 = undergrad, 1 = grad, 2 = doctoral student, 3 = postdoc
+    String requirements; // required GPA to apply
     String phone; // contact phone
     String contact; // contact person
-    HashList<Student> employees; // list of employees
 
-    public Job() {
-//        creator = ;
-//        type = ;
-//        majors.add();
-//        title = ;
-//        desc = ;
-//        gpa = ;
-//        classYear = ;
-//        email = ;
-
+    public Job(String id, String title, String department, String location, int numStudents, int hours, String email, boolean federalWorkStudy, String desc, String requirements, String phone, String contact) {
+        this.id = id;
+        this.title = title;
+        this.department = department;
+        this.location = location;
+        this.numStudents = numStudents;
+        this.hours = hours;
+        this.email = email;
+        this.federalWorkStudy = federalWorkStudy;
+        this.desc = desc;
+        this.requirements = requirements;
+        this.phone = phone;
+        this.contact = contact;
     }
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getNumStudents() {
+        return numStudents;
+    }
+
+    public void setNumStudents(int numStudents) {
+        this.numStudents = numStudents;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isFederalWorkStudy() {
+        return federalWorkStudy;
+    }
+
+    public void setFederalWorkStudy(boolean federalWorkStudy) {
+        this.federalWorkStudy = federalWorkStudy;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(String requirements) {
+        this.requirements = requirements;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
 }
