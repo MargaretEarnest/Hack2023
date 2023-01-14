a9f3db13f280371946f3b5df866ad74b91fbd9ea
Anthony Vuolo <vuolo.969473@gmail.com>
2023-01-14 02:07:38 -0500

    Added field to Job.java

--- backend/src/Job.java
+++ backend/src/Job.java
@@ -12,7 +12,7 @@ specific job attributes including:
 
 public class Job {
     int id; // unique id for this job
-    String creator; // username of the employer who created the job
+    HashList<Employer> creators; // employers overseeing the job
     int type; // 0=research, 1=TA, 2=other
     HashList<String> majors; // majors required for student
     String title; // name of the job position
@@ -25,9 +25,10 @@ public class Job {
     String desc; // description of the job position
     float gpa; // required GPA to apply
     int classYear; // required class year
-    int grad; // 0 = undergrad, 1 = grad, 2 = postdoc
-    String phone;
+    int grad; // 0 = undergrad, 1 = grad, 2 = doctoral student, 3 = postdoc
+    String phone; // contact phone
     String contact; // contact person
+    HashList<Student> employees; // list of employees
 
     public Job() {
 //        creator = ;
