4b17f8a764113ae14bb2699e75de79fd73491970
Jakob Misbach <jakobmisbach8@gmail.com>
2023-01-14 00:28:59 -0500

    Added more json object

--- hack-2023/src/jsonObjects/Student.ts
+++ hack-2023/src/jsonObjects/Student.ts
@@ -4,20 +4,20 @@ export class Student {
     private fName: string;
     private lName: string;
     private suffix: string;
-    private type: number;
+    private status: number;
     private majors: string[];
     private yearOfGraduation: number;
     private gpa: number;
     private classes: string[];
 
 
-    constructor(email: string, prefix: string, fName: string, lName: string, suffix: string, type: number, majors: string[], yearOfGraduation: number, gpa: number, classes: string[]) {
+    constructor(email: string, prefix: string, fName: string, lName: string, suffix: string, status: number, majors: string[], yearOfGraduation: number, gpa: number, classes: string[]) {
         this.email = email;
         this.prefix = prefix;
         this.fName = fName;
         this.lName = lName;
         this.suffix = suffix;
-        this.type = type;
+        this.status = status;
         this.majors = majors;
         this.yearOfGraduation = yearOfGraduation;
         this.gpa = gpa;
